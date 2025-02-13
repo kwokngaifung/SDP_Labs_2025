97938daf61a1b2bf8b17962eb57ff25557b8e51f
kwokngaifung <240098761@stu.vtc.edu.hk>
2025-02-13 12:19:49 +0800

    1 1

--- SDP_Labs_2025/frmLab1.cs
+++ SDP_Labs_2025/frmLab1.cs
@@ -22,8 +22,8 @@ namespace SDP_Labs_2025
         {
             try
             {
-                int x = Convert.ToInt32(txtNum1.Text) + Convert.ToInt32(txtNum2.Text);
-                txtNum3.Text = Convert.ToString(x);
+                int sum = Convert.ToInt32(txtNum1.Text) + Convert.ToInt32(txtNum2.Text);
+                txtNum3.Text = Convert.ToString(sum);
             }
             catch (Exception ex) 
             {
