71c76ab4e7c795c8e9ca2d583462e286565bad87
kwokngaifung <240098761@stu.vtc.edu.hk>
2025-03-13 11:26:08 +0800

    1

---
+++ SDP_Labs_2025/Lab4/frmLab4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDP_Labs_2025.Lab4
+{
+    public partial class frmLab4 : Form
+    {
+        public frmLab4()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SDP_WebAPI/Controllers/SimpleGetAPIController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SDP_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleGetAPIController : ControllerBase
+    {
+        [HttpGet("SimpleGetMethod")]
+        public String Get() {
+            return $"This is returned by the web API server. Current time is: {DateTime.Now}";
+        }
+    }
+}
