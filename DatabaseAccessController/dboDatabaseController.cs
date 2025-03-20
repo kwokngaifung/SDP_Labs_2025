af3408589039ee2784c246904b825535e1b0a511
kwokngaifung <240098761@stu.vtc.edu.hk>
2025-03-20 10:59:25 +0800

    Lab5 1.0

---
+++ DatabaseAccessController/dboDatabaseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseAccessController
+{
+    public class dboDatabaseController
+    {
+        private String connectionString { get; set; }
+
+        public dboDatabaseController(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable GetData(String sqlCmd)
+        {
+            // Create the connection (and be sure to dispose it at the end)
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the connection to the database. 
+                    // This is the first critical step in the process.
+                    conn.Open();
+                    MySqlDataAdapter adr = new MySqlDataAdapter(sqlCmd, conn);
+                    DataTable dt = new DataTable();
+                    adr.Fill(dt);
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                    //log the error 
+                }
+            }
+        }
+
+    }
+}
---
+++ DatabaseAccessController/dboGetCompanyData.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace DatabaseAccessController
+{
+    public class dboGetCompanyData : dboDatabaseController
+    {
+        public dboGetCompanyData(string connectionString) : base(connectionString)
+        {
+        }
+
+        public DataTable GetAllCustomerData()
+        {
+            String sqlCmd = "SELECT * FROM customers";
+            return GetData(sqlCmd);
+        }
+    }
+}
