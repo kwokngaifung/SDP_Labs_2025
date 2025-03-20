cd50b873b9716a2a68a3053139ed153d8ca184fc
kwokngaifung <240098761@stu.vtc.edu.hk>
2025-03-20 11:21:38 +0800

    Lab5 1.1

--- SDP_WebAPI/Controllers/SimpleGetAPIController.cs
+++ SDP_WebAPI/Controllers/SimpleGetAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SDP_WebAPI.Controllers
@@ -7,9 +8,30 @@ namespace SDP_WebAPI.Controllers
     [ApiController]
     public class SimpleGetAPIController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public SimpleGetAPIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet("SimpleGetMethod")]
         public String Get() {
             return $"This is returned by the web API server. Current time is: {DateTime.Now}";
         }
+
+        [HttpGet("GetCustomerData")]
+        public String GetCustomerData()
+        {
+            dboGetCompanyData dboGetCompanyData = new dboGetCompanyData(_configuration["ConnectionStrings"]);
+            DataTable dtResult = dboGetCompanyData.GetAllCustomerData();
+
+            // Convert DataTable to JSON string
+            string jsonString = JsonConvert.SerializeObject(dtResult);
+
+            // Return JSON string
+            return jsonString;
+        }
+
     }
 }
